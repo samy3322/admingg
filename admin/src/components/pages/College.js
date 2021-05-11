76549b5dc248ce97979b9752d5a724f807319bd9
samy3322 <77373469+samy3322@users.noreply.github.com>
2021-05-11 11:52:05 +0530

    Update College.js
    as

--- admin/src/components/pages/College.js
+++ admin/src/components/pages/College.js
@@ -19,7 +19,7 @@ function College()
     const[placement,setPlacement]=useState("null");
     const[average,setAverage]=useState("");
     const[affiliated_to,setAffiliated_to]=useState("");
-    const[certificate,setCertificate]=useState("");
+    const[certficate,setCertficate]=useState("");
 
     const [cno1Error, setCno1Error] = useState({})
 
@@ -56,7 +56,7 @@ function College()
   
  
   
-const fileType=['application/pdf'];
+/*const fileType=['application/pdf'];
 const handlepdf=(e)=>{
   let selectedpdf=e.target.files[0];
   if(selectedpdf)
@@ -80,6 +80,8 @@ else{
   console.log('select your file');
 }
 }
+
+*/
 const onSubmit=(e)=>
 {
   e.preventDefault();
@@ -167,7 +169,7 @@ return isValid;
     alert('Enter Affiliated details');
     return false;
 }
-if (document.getElementsByName('certificate')[0].value === '') {
+if (document.getElementsByName('certficate')[0].value === '') {
   alert('Enter Certificate ');
   return false;
 }
@@ -178,7 +180,7 @@ async function regi()
     {
       if(formValidation1())
     {
-       let item={c_name,clocation,caddress,cno1,cno2,cemail_id,about,academic,accommodation,faculty,placement,average,affiliated_to,certificate}
+       let item={c_name,clocation,caddress,cno1,cno2,cemail_id,about,academic,accommodation,faculty,placement,average,affiliated_to,}
     console.warn(item)
     let result=await fetch("http://127.0.0.1:8000/api/clgadd",{
         method:'post',
@@ -247,7 +249,7 @@ return(
          <div className="form-group"><input  className="form-control form-control-lg" type="text"  name="affiliated_to" value={affiliated_to}   placeholder=" ENTER THE Affiliated Field "  onChange={(e)=>setAffiliated_to(e.target.value)}/><br/></div>
          
         
-         <div className="form-group">upload verfied certificate of college<input type="file" className="form-control form-control-lg"   name="certificate" value={certificate}   placeholder="select the certificate"  onChange={(e)=>setCertificate(e.target.value)}/><br/></div>
+         <div className="form-group">upload verfied certficate of college<input type="file" className="form-control form-control-lg"   name="certficate" value={certficate}   placeholder="select the certficate"  onChange={(e)=>setCertficate(e.target.value)}/><br/></div>
          {pdfFileError&&<div className='form-control form-control-lg'>{pdfFileError}</div>}
      <div className="pdf-container"></div>
         <br/>
