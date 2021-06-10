dc797236f234419d91f29f5f6c58f9b3247dd627
abi-shetty <abhishekshetty951@gmail.com>
2021-06-10 16:18:16 +0530

    added search for delete course in abi

--- admin/src/components/abi/Delcourse.js
+++ admin/src/components/abi/Delcourse.js
@@ -1,19 +1,30 @@
 import { useEffect, useState ,Link } from "react";
 import { useHistory } from 'react-router-dom';
-
+import SearchField from "react-search-field";
+import axios from 'axios';
 const Delcourse = () => {
     const history=useHistory();
-    const [data, setdata] = useState(null)
+    const [data, setdata] = useState(null);
+    const [ispending, setispending] = useState(true);
     useEffect(() => {
         fetch('http://127.0.0.1:8000/api/courseshow')
           .then(res => {
             return res.json();
+
           })
           .then(data => {
             setdata(data);
             console.log(data);
+            setispending(false);
           })
       }, [])
+      const handlesearch=(e)=>{
+        setispending(true);
+        axios.get(`http://127.0.0.1:8000/api/searchcourse/`+e)
+        .then(res => {
+            setdata(res.data);
+            setispending(false);})
+      }
       const handleclick=(e)=>{
         const id=e.target.value;
         //e.preventDefault();
@@ -28,19 +39,21 @@ const Delcourse = () => {
         }).then(() => {
           console.log('Deleted');
           alert("Course Deleted")
+          window.location.reload();
     })
     }}
-    if(data != null &&data.length===0)
-    {
-        return(
-            <div className="te">
-                <h1>No Results</h1>
-            </div>
-        );
-
-    }
     return (
         <div className="clist">
+             <SearchField
+  placeholder="Search..."
+  onChange={(e) => handlesearch(e)}
+  searchText=""
+  classNames=""
+/>  {ispending && <div className="pending">
+        Loading....
+      </div>}
+      {data &&!data.length&& <div className="nores"><h3>No Results</h3></div>
+      }
       {data && data.map(dat => (
         <div className="litview" key={dat.course_id} >
           <h2>Course Name  { dat.course }</h2>
