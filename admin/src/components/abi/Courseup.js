a0ba53774d72ca70f6500afe659444abf06b2c5c
abi-shetty <abhishekshetty951@gmail.com>
2021-06-10 16:26:23 +0530

    added search to update course in abi

--- admin/src/components/abi/Courseup.js
+++ admin/src/components/abi/Courseup.js
@@ -1,9 +1,11 @@
 import { useEffect, useState ,Link } from "react";
 import { useHistory } from 'react-router-dom';
-
+import SearchField from "react-search-field";
+import axios from 'axios';
 const Courseup = () => {
     const history=useHistory();
     const [data, setdata] = useState(null)
+    const [ispending, setispending] = useState(true);
     useEffect(() => {
         fetch('http://127.0.0.1:8000/api/courseshow')
           .then(res => {
@@ -12,23 +14,32 @@ const Courseup = () => {
           .then(data => {
             setdata(data);
             console.log(data);
+            setispending(false);
           })
       }, [])
+      const handlesearch=(e)=>{
+        setispending(true);
+        axios.get(`http://127.0.0.1:8000/api/searchcourse/`+e)
+        .then(res => {
+            setdata(res.data);
+            setispending(false);})
+      }
       const handleclick=(e)=>{
         //console.log(e.target.value);
         history.push("/Courseup2/"+e.target.value);
     }
-    if(data != null &&data.length===0)
-    {
-        return(
-            <div className="te">
-                <h1>No Results</h1>
-            </div>
-        );
-
-    }
     return (
         <div className="clist">
+          <SearchField
+  placeholder="Search..."
+  onChange={(e) => handlesearch(e)}
+  searchText=""
+  classNames=""
+/>  {ispending && <div className="pending">
+        Loading....
+      </div>}
+      {data &&!data.length&& <div className="nores"><h3>No Results</h3></div>
+      }
       {data && data.map(dat => (
         <div className="litview" key={dat.course_id} >
           <h2>Course Name  { dat.course }</h2>
